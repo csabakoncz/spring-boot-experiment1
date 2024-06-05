c8c826fb0b790a0a7efef5b24a3a6a6e41adff2c
Csaba Koncz <ckoncz@cloudera.com>
2024-06-05 18:26:12 +0200

    Custom JPA queries (including nativeQuery)

---
+++ src/test/java/com/ck/b1/model/CustomerRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.ck.b1.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest()
+public class CustomerRepositoryTest {
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Test
+	void canCreateAndFindCustomers() {
+		var c1 = new Customer("First1", "Csaba");
+		var c2 = new Customer("Csaba", "Last2");
+		var c3 = new Customer("First3", "Last3");
+
+		customerRepository.save(c1);
+		customerRepository.save(c2);
+		customerRepository.save(c3);
+
+		Assertions.assertEquals(3, customerRepository.count());
+
+		// H2 seems case sensitive
+		var csabaList = customerRepository.findByName("Csaba");
+		Assertions.assertEquals(2, csabaList.size());
+
+		var csabaListIC = customerRepository.findByNameIC("cSaba");
+		Assertions.assertEquals(2, csabaListIC.size());
+	}
+
+}
